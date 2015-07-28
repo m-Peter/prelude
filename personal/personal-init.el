2e3a4609bbc2ccd9ed6655c45167c0ff960bbb3d
Petros Markou <markoupetr@skroutz.gr>
2015-07-28 10:31:46 +0300

    Remove OS X specifics

--- personal/personal-init.el
+++ personal/personal-init.el
@@ -1,9 +1,6 @@
 ;;; personal-init.el --- Custom emacs configuration
 
 ;;; Code:
-;; Swap super with meta (useful on  OS X)
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'super)
 
 ;; Enable caching
 (setq projectile-enable-caching t)
