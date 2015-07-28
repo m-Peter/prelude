59cad9209b02fa2b61b7d5ef7f2bc5d95cd0b0d7
Petros Markou <markoupetr@skroutz.gr>
2015-07-28 11:07:25 +0300

    Fix for removing annoying prompt

--- personal/personal-init.el
+++ personal/personal-init.el
@@ -8,5 +8,7 @@
 ;; Disable magit setup instructions
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+(setq enable-local-variables :safe)
+
 (provide 'personal-init)
 ;;; personal-init.el ends here
