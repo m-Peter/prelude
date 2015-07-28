372da427a16cd889511d4fb6f9afae28a718b169
Petros Markou <markoupetr@skroutz.gr>
2015-07-28 11:35:30 +0300

    Add more configs in personal-ui

--- personal/personal-ui.el
+++ personal/personal-ui.el
@@ -1,8 +1,41 @@
-(global-linum-mode t)
-;;(setq default-frame-alist '((font . "Fira Mono-14")))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(menu-bar-mode -1)
+
+(blink-cursor-mode -1)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; more useful frame title, that show either a file or a
+;; buffer name (if the buffer isn't visiting a file)
+(setq frame-title-format
+      '("" invocation-name " Prelude - " (:eval (if (buffer-file-name)
+                                                    (abbreviate-file-name (buffer-file-name))
+                                                  "%b"))))
+
+(require 'smart-mode-line)
+(setq sml/no-confirm-load-theme t)
+;; delegate theming to the currently active theme
+(setq sml/theme nil)
+(add-hook 'after-init-hook #'sml/setup)
 
 ;; enable monokai theme
-(load-theme 'bliss t)
+(load-theme 'tronesque t)
 
 ;; disable scroll bar
 (scroll-bar-mode -1)
