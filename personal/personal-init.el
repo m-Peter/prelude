65327ce872651c10a187e76c8f4d6bd8b2a7fc5e
Petros Markou <markoupetr@skroutz.gr>
2015-11-20 08:55:33 +0200

    Enable meta and super modifier for OS X

--- personal/personal-init.el
+++ personal/personal-init.el
@@ -5,6 +5,10 @@
 ;; Enable caching
 (setq projectile-enable-caching t)
 
+;; Swap super with meta (useful on  OS X)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
 ;; Disable magit setup instructions
 (setq magit-last-seen-setup-instructions "1.4.0")
 
